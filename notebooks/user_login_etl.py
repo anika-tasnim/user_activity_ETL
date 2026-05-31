f01b109432ef6454699b01a86409a16a90e846cb
anika-tasnim <39561058+anika-tasnim@users.noreply.github.com>
2026-05-31 16:24:00 +0200

    user_activity_etl.py
    Read JSON data, perform data cleaning, and store the transformed dataset in Databricks.

--- notebooks/user_login_etl.py
+++ notebooks/user_login_etl.py
@@ -1,1 +1,18 @@
 # Databricks ETL Notebook
+# Loading the json file into spark.
+df = spark.read.option("multiline", "true").json("/Volumes/workspace/default/raw_data/user_activity_data.json")
+display(df)
+
+# Checking all the schemas.
+df.printSchema()
+
+# Cleaning the data. Dropping the duplicate entries and saving the new table.
+df_clean = df.dropDuplicates()  
+display(df_clean) 
+df_clean.write.mode("error").saveAsTable("usr_activity_cleaned_data")
+
+# Show spark tables
+spark.sql("SHOW TABLES").show()
+
+# Describe new table
+spark.sql("DESCRIBE TABLE usr_activity_cleaned_data").show(truncate=False)
